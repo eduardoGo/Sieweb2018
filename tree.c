f54ff5785733b99a2478dc9c07e8324e5cfd00d7
eduardoGo <egsantos128@gmail.com>
2017-09-14 11:29:02 -0300

    Create tree.c

---
+++ tree.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TREE tree;
+
+
+struct TREE{
+	unsigned char byte;
+	int frequency;
+	tree next;
+	tree left;
+	tree right;
+};
+/*
+	Somente adciona o nó na frente da lista.
+  Retorna um poteiro para o nó que foi adcionado
+*/
+tree* add_element(tree *tree, unsigned char byte, int frequency)
+{
+	tree *temp = (tree*) malloc(1*sizeof(tree));
+	temp->left = NULL;
+	temp->right = NULL;
+	temp->next = NULL;
+	tree->next = temp;
+	temp->byte = byte;
+	temp->frequency = frequency;
+	return temp;
+}
+//FALTA O RESTO
