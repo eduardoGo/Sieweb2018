654f1b39e3aa2494cce95ba0c6d0406143300b0f
eduardoGo <egsantos128@gmail.com>
2017-09-18 17:05:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	FILE *arq;
+	if(argc == 3)
+	{
+		if(argv[1] == 'c')
+		{
+			arq = fopen(argv[2],"rb");
+			compress();
+		}
+		else if(argv[1] == 'd')
+		{
+			arq = fopen(argv[2],"rb");
+			descompress();
+		}
+	}
+	else
+	{
+		printf("Argumentos inválidos, ultilize o formato < c > < file >");
+		return 0;
+	}
 	return 0;
 }
